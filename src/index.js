47d5514d277af765217748b1ba5953c395da45b5
thomasneo <thomaslossio@hotmail.com>
2019-04-05 22:24:48 -0300

    Modulo concluído

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-const soma = (a, b) => a + b;
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
 
-alert(soma(6, 3));
+import Button from './Button';
+
+import './style.scss';
+
+class App extends Component {
+  state = {
+    counter: 0,
+  };
+
+  handleClick = () => {
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <Fragment>
+        <h1 className="title">Hello RocketSeat</h1>
+        <h2 style={{ color: '#f00' }}>{counter}</h2>
+        <Button onClick={this.handleClick}>Somar</Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
